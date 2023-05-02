5444660ef9f251907ccb27f5145d0ba500131b4d
Ikwuegbuenyi Augustus Meju <101446446+Imejuart1@users.noreply.github.com>
2023-05-02 11:47:14 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,21 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import process from 'process';
 
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
+// Initialize Firebase project configuration
 const firebaseConfig = {
-  // ...
-  apiKey: "AIzaSyDHp5maA9CFj4UkXe_xzlEiRtt0HxKfHss",
-  authDomain: "pipureport-6b980.firebaseapp.com",
-  projectId: "pipureport-6b980",
-  storageBucket: "pipureport-6b980.appspot.com",
-  messagingSenderId: "246680606543",
-  appId: "1:246680606543:web:d518f2534cbfd5b45a173a"
-    
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
