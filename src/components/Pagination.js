90a11b46880f6d0c4aa2647708f8bf8836c8bf9e
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2023-05-02 08:59:34 +0100

    Login page responsive

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,16 +1,19 @@
+//This component is for the results displayed in the home page
 import "./Pagination.css"
 
 function Pagination({ itemsPerPage, totalItems, currentPage, setCurrentPage }) {
+  // Determine the total number of pages
   const pageNumbers = [];
-
   for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
     pageNumbers.push(i);
   }
 
+  // Determine the range of items currently being displayed
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentShowing = `${indexOfFirstItem + 1}-${indexOfLastItem > totalItems ? totalItems : indexOfLastItem} / ${totalItems}`;
 
+  // Render the pagination buttons
   return (
     <nav className="pagination-container">
       <div className="showing-container">{currentShowing}</div>
@@ -29,6 +32,5 @@ function Pagination({ itemsPerPage, totalItems, currentPage, setCurrentPage }) {
     </nav>
   );
 }
-
 
 export default Pagination
