7deca1e8fa1d7f6c0178edf98f051576f47b35ca
Ikwuegbuenyi Augustus Meju <101446446+Imejuart1@users.noreply.github.com>
2023-05-04 18:52:52 +0100

    Update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -13,12 +13,30 @@ function Pagination({ itemsPerPage, totalItems, currentPage, setCurrentPage }) {
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentShowing = `${indexOfFirstItem + 1}-${indexOfLastItem > totalItems ? totalItems : indexOfLastItem} / ${totalItems}`;
 
+  // Determine the range of pages to show
+  const maxPagesToShow = 10;
+  const halfMaxPagesToShow = Math.floor(maxPagesToShow / 2);
+  let startPage = Math.max(1, currentPage - halfMaxPagesToShow);
+  let endPage = Math.min(startPage + maxPagesToShow - 1, pageNumbers.length);
+
+  if (endPage - startPage < maxPagesToShow - 1) {
+    startPage = Math.max(1, endPage - maxPagesToShow + 1);
+  }
+
   // Render the pagination buttons
   return (
     <nav className="pagination-container">
       <div className="showing-container">{currentShowing}</div>
       <ul className="pagination">
-        {pageNumbers.map((number) => (
+        {startPage > 1 && (
+          <li key="first" className="page-item">
+            <button className="page-link" onClick={() => setCurrentPage(1)}>
+              1
+            </button>
+          </li>
+        )}
+        {startPage > 2 && <li key="ellipsis-start">...</li>}
+        {pageNumbers.slice(startPage - 1, endPage).map((number) => (
           <li key={number} className="page-item">
             <button
               className={number === currentPage ? "page-link active" : "page-link"}
@@ -28,6 +46,14 @@ function Pagination({ itemsPerPage, totalItems, currentPage, setCurrentPage }) {
             </button>
           </li>
         ))}
+        {endPage < pageNumbers.length - 1 && <li key="ellipsis-end">...</li>}
+        {endPage < pageNumbers.length && (
+          <li key="last" className="page-item">
+            <button className="page-link" onClick={() => setCurrentPage(pageNumbers.length)}>
+              {pageNumbers.length}
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
