97b25da65ae6e5b27a8e1354ed2fb45c987e597f
Ikwuegbuenyi Augustus Meju <101446446+Imejuart1@users.noreply.github.com>
2023-05-07 22:25:26 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Initialize Firebase project configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDHp5maA9CFj4UkXe_xzlEiRtt0HxKfHss",
-  authDomain: "pipureport-6b980.firebaseapp.com",
-  projectId: "pipureport-6b980",
-  storageBucket: "pipureport-6b980.appspot.com",
-  messagingSenderId: "246680606543",
-  appId: "1:246680606543:web:d518f2534cbfd5b45a173a"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
