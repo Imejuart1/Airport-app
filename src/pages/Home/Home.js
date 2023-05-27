80d709cf79ea05a80d82c2a0d85c065e9f5817f7
Ikwuegbuenyi Augustus Meju <101446446+Imejuart1@users.noreply.github.com>
2023-05-27 08:57:22 +0100

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -81,7 +81,7 @@ function Home() {
   // Pagination logic
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = Object.keys(airportCounts || {}).slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = Object.keys(airportCounts.currentTime|| {}).slice(indexOfFirstItem, indexOfLastItem);
 
 
 return (
