adbef92036d3864493b46faf452863d723f9ba1a
Ikwuegbuenyi Augustus Meju <101446446+Imejuart1@users.noreply.github.com>
2023-08-10 03:33:37 +0100

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -97,21 +97,8 @@ return (
         </tr>
       </thead>
       <tbody>
-  <p> Harvest committee meeting of 27th June, 2023.
-Meeting started @8:46am with prayers from Mr. Asulemen. Followed by a word of encouragement from the harvest chairman. She went further to tell members the reason for the meeting which is because of the forth coming covenant prayer with the Lord. She encourage all members to take the message outside and invite many people for our upcoming  program beginning on Wednesday 28 to 30th June, 2023
-
-Mr Asulemen further encourage member's participation and the need for frequent physical meeting and minutes of our meetings to be properly documented for ease of getting information across to all members and emphasized the importance of carrying all members along.
- 
-The second discussion was to assess performance of our programs so far. The chairman made the house to understand that having done the harvest Theme selection and unveiling that we have not made up to a 1m naira, recalled our target is 250m,  hence the need for all members to step up our actions towards mobilizing people for all our programs especially the forthcoming covenant with the Lord program that can give the church money for the harvest. For the program, we agreed that the chairman should contact the church choir and charismatic, inform them to take charge of praise/worship during the program. The following conclusions were also reached: Security:- @Youth president Segun Ola should make arrangement to mount the gate till the main night security resume.
-Mass servers:- Augustus to inform the Mass servers for the program.
-Media:- Stephen Iji should follow up on projector to be used.
-Clean up of the church:- all harvest members to ensure the chairs are arranged and church premises be clean each day.
-We also agreed the chairman and Barrister Chimdi should meet Padre to roll out program for the forthcoming event. 
-
-On members that are not showing interest for our meetings/activities: we agreed to get the master list of members from Padre and people there to be contacted and those that are not ready should be replace to enable us achieve our aim for the harvest.
-
-On finance: The financial secretory sent her records to the meeting with the excuses that she would be away for sometimes and the finance book was giving back to Mr Anaga
-        {/* Map through the list of airport codes and display the corresponding counts */}</p>
+  
+        {/* Map through the list of airport codes and display the corresponding counts */}
         {currentItems.map((airportCode) => (
           <tr key={airportCode}>
             <td>{airportCode}</td>
